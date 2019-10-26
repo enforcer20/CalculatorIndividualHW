4a4967693de2aa52b48a63fad4163234e3fb4d39
Karan <kwalanj@gmail.com>
2019-10-25 21:34:51 -0400

    Feature: Create CSVTests file

---
+++ src/CSVTests.py
@@ -0,0 +1,16 @@
+import unittest
+from CsvReader import CsvReader
+from pprint import pprint
+
+
+class MyTestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.csv_reader = CsvReader('Addition.csv')
+
+    def test_instantiate_calculator(self):
+        self.assertIsInstance(self.csv_reader, CsvReader)
+
+
+if __name__ == '__main__':
+    unittest.main()
