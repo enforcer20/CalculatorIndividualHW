01cccf9bddac82a8d9f9c49ae61b3e216f0125ad
Karan <kwalanj@gmail.com>
2019-10-27 14:13:42 -0400

    Fix: Cleaned up the code and removed commented/test lines

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -11,9 +11,6 @@ class MyTestCase(unittest.TestCase):
     def test_instantiate_calculator(self):
         self.assertIsInstance(self.calculator, Calculator)
 
-    ##  def test_results_property_calculator(self):
-    ##     self.assertEqual(self.calculator.result, 6)
-
     def test_add_method_calculator(self):
         test_data = CsvReader('./src/Addition.csv').data
         for row in test_data:
@@ -40,16 +37,6 @@ class MyTestCase(unittest.TestCase):
             z = float(row['Result'])
             self.assertEqual(self.calculator.divide(x, y), round(z, 7))
             self.assertEqual(self.calculator.result, round(z, 7))
-    #     expected_result = int(row['Result'])
-    #    result = Calculator.divide(a, b)
-    #   new_division = round(result, 7)
-    #  self.assertEqual(new_division, expected_result)
-
-    # self.assertEqual(self.calculator.divide(a, b), int(row['Result']))
-
-    # Result = divide(a, b)
-    # self.assertEqual(self.calculator.divide(row['Value 1'], row['Value 2']), int(row['Result']))
-    # self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_square_method_calculator(self):
         test_data = CsvReader('./src/Square.csv').data
@@ -64,10 +51,6 @@ class MyTestCase(unittest.TestCase):
             y = float(row['Result'])
             self.assertEqual(self.calculator.sqrt(x), round(y, 8))
             self.assertEqual(self.calculator.result, round(y, 8))
-        #   self.assertEqual(self.calculator.sqrt(row['Value 1']), int(row['Result']))
-        #    self.assertEqual(self.calculator.result, int(row['Result']))
-        # self.assertEqual(self.calculator.sqrt(4), 2)
-        # self.assertEqual(self.calculator.result, 2)
 
 
 if __name__ == '__main__':
